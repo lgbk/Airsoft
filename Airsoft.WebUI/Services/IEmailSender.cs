d3963b44abcecb0c52a66e1a202ee0f7ec7e117e
lgbkk <lgbkk@QUANTUM>
2016-06-12 00:52:11 +1000

    Changed some text

---
+++ Airsoft.WebUI/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airsoft.WebUI.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
